e222305c19a199f92fffd2cb59472c46a52bab9f
Andres Weber <andresmweber@gmail.com>
2019-08-06 00:12:48 -0400

    Refactored to classes to make instances more easily controllable (could've stayed with object based for Board, but like initializing values at a whim.  Moved command running to be owned by the Board so we can make orderly turns and store lists of commands for each rover to be run per turn and so the board can validate any move.

--- src/rover.js
+++ src/rover.js
@@ -1,9 +1,17 @@
-var _ = require('underscore');
+function arraySum(array1, array2, array_size = 2) {
+    var array_result = Array()
+    for (i = 0; i < array_size; i++) {
+        array_result.push(array1[i] + array2[i])
+    }
+    return array_result
+}
 
-// Rover Object Goes Here
-// ======================
-const board_width = 10
-const start_pos = [0, 0]
+const roverRender = {
+    'N': '^',
+    'E': '>',
+    'S': 'v',
+    'W': '<',
+}
 
 const directionLUT = {
     'N': [0, -1],
@@ -26,61 +34,55 @@ const commandsLUT = {
     'b': 'moveBackward'
 }
 
-const rover = {
-    _direction: 0,
-    position: start_pos,
-    travel_log: [start_pos],
+// Rover Object Goes Here
+// ======================
+class Rover {
+    constructor(name, start_pos, direction) {
+        this.name = name
+        this.position = start_pos
+        this.travel_log = [start_pos]
+        this._direction = direction;
+    }
 
     get direction() {
         return compass[this._direction]
-    },
-
-    runCommands(commands) {
-        commands.trim().split('').forEach((command) => {
-            let valid_command = commandsLUT[command]
-            if (valid_command) {
-                this[valid_command]()
-            } else {
-                console.log(`Invalid command entered: ${command}`)
-            }
-        })
-    },
+    }
 
     makeTurn(direction) {
         this._direction = this._remapDirectionLookup(this._direction + direction)
         console.log(`Made a turn: new direction is ${this.direction}`)
-    },
+    }
 
     turnLeft() {
         this.makeTurn(-1)
-    },
+    }
 
     turnRight() {
         this.makeTurn(1)
-    },
+    }
 
     moveForward() {
         console.log('Moving forward!')
         this.move(this.direction)
-    },
+    }
 
     moveBackward() {
         console.log('Moving backward!')
         this.move(compass[this._remapDirectionLookup(this._direction + 2)])
-    },
+    }
 
     move(direction) {
         console.log(`Moving in direction ${directionLUT[direction]} while facing ${this.direction}`)
         try {
             let array = arraySum(this.position, directionLUT[direction])
-            this.position = board.inBounds(array)
+            this.position = board.validMove(array)
         } catch (error) {
-            console.log('Illegal move attempted, staying put.')
+            console.log(`Illegal move attempted, staying put: ${error.message}`)
         } finally {
             console.log(`Final position is ${this.position}`)
             this.travel_log.push(this.position)
         }
-    },
+    }
 
     _remapDirectionLookup(directionIndex) {
         var modifier = 0
@@ -90,47 +92,57 @@ const rover = {
             modifier = -4
         }
         return directionIndex + modifier
-    },
-
-}
-
-function arraySum(array1, array2, array_size = 2) {
-    var array_result = Array()
-    for (i = 0; i < array_size; i++) {
-        array_result.push(array1[i] + array2[i])
     }
-    return array_result
 }
 
-const board = {
-    roverRender: {
-        'N': '^',
-        'E': '>',
-        'S': 'v',
-        'W': '<',
-    },
-    turn: 0,
-    rovers: [],
 
-    inBounds(pos) {
+class Board {
+    constructor(tiles = 10) {
+        this.board_width = tiles,
+            this.turn = 0
+        this.player = new Rover('Player', [board_width / 2, board_width / 2], 0),
+            this.rovers = self.populate_rovers()
+    }
+
+    validMove(pos) {
         pos.forEach((coordinate) => {
-            if (board_width / 2 > coordinate && coordinate < 0) {
+            if (this.board_width / 2 > coordinate && coordinate < 0) {
                 throw new Error('Out of bounds move.')
             }
         })
+        this.rovers.forEach(rover => {
+            if (rover.position[0] === pos[0] && rover.position[1] === pos[0]) {
+                throw new Error('Space is occupied by another rover.')
+            }
+        })
+        this.obstacles.forEach(obstacle => {
+            if (obstacle[0] === pos[0] && obstacle[1] === pos[0]) {
+                throw new Error('Space is occupied by an obstacle!')
+            }
+        })
         return pos
-    },
+    }
 
-    printRoverTravelLog(rover_index) {
+    _parseCommands(commands) {
+        return commands.trim().split('').filter((command) => commandsLUT[command] !== undefined)
+    }
+
+    runTurn(commands) {
+        this._parseCommands(commands).forEach((command) => {
+            this[valid_command]()
+        })
+    }
+
+    displayRoverTravelLog(rover_index) {
         this.printBoard(this.rovers[rover_index])
-    },
+    }
 
-    printBoard(roverLogs = []) {
+    displayBoard(roverLogs = []) {
         if (roverLogs.length) console.log('\n\nPrinting travel log:\n')
 
-        for (i = 0; i < board_width; i++) {
+        for (i = 0; i < this.board_width; i++) {
             let row = []
-            for (j = 0; j < board_width; j++) {
+            for (j = 0; j < this.board_width; j++) {
                 if (roverLogs.length) {
                     row[j] = '[   ]'
                     roverLogs.forEach((roverLog) => {
@@ -144,10 +156,6 @@ const board = {
             }
             console.log(row.join(' '))
         }
-    },
-
-    sendCommands(commands, roverIndex) {
-        this.rovers[roverIndex].runCommands(commands)
     }
 }
 
