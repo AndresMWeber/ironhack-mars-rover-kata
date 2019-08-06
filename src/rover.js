8d3a3bbda7e7b409df84dbd46468edf5fcd6a11e
Andres Weber <andresmweber@gmail.com>
2019-08-05 21:39:47 -0400

    Added a bunch of functionality through Bonus 3.  Refactoring board from here on to fit their Bonus 4

--- src/rover.js
+++ src/rover.js
@@ -3,11 +3,12 @@ var _ = require('underscore');
 // Rover Object Goes Here
 // ======================
 const board_width = 10
+const start_pos = [0, 0]
 
-const direction_LUT = {
-    'N': [0, 1],
+const directionLUT = {
+    'N': [0, -1],
     'E': [1, 0],
-    'S': [0, -1],
+    'S': [0, 1],
     'W': [-1, 0],
 }
 
@@ -18,22 +19,35 @@ const compass = {
     3: 'W'
 }
 
+const commandsLUT = {
+    'f': 'moveForward',
+    'r': 'turnRight',
+    'l': 'turnLeft',
+    'b': 'moveBackward'
+}
+
 const rover = {
     _direction: 0,
-    position: [0, 0],
+    position: start_pos,
+    travel_log: [start_pos],
+
     get direction() {
         return compass[this._direction]
     },
+
+    runCommands(commands) {
+        commands.trim().split('').forEach((command) => {
+            let valid_command = commandsLUT[command]
+            if (valid_command) {
+                this[valid_command]()
+            } else {
+                console.log(`Invalid command entered: ${command}`)
+            }
+        })
+    },
+
     makeTurn(direction) {
-        // should only be -1 for left +1 for right
-        let tmp_direction = this._direction + direction
-        var modifier = 0
-        if (tmp_direction < 0) {
-            modifier = 4
-        } else if (direction > 3) {
-            modifier = -4
-        }
-        this._direction = tmp_direction + modifier
+        this._direction = this._remapDirectionLookup(this._direction + direction)
         console.log(`Made a turn: new direction is ${this.direction}`)
     },
 
@@ -46,20 +60,41 @@ const rover = {
     },
 
     moveForward() {
-        console.log('moveForward was called!', this.direction)
+        console.log('Moving forward!')
+        this.move(this.direction)
+    },
+
+    moveBackward() {
+        console.log('Moving backward!')
+        this.move(compass[this._remapDirectionLookup(this._direction + 2)])
+    },
+
+    move(direction) {
+        console.log(`Moving in direction ${directionLUT[direction]} while facing ${this.direction}`)
         try {
-            let array = arraySum(this.position, direction_LUT[this.direction])
+            let array = arraySum(this.position, directionLUT[direction])
             this.position = this._inBounds(array)
         } catch (error) {
             console.log('Illegal move attempted, staying put.')
         } finally {
             console.log(`Final position is ${this.position}`)
+            this.travel_log.push(this.position)
+            printBoard()
         }
+    },
+
+    _remapDirectionLookup(directionIndex) {
+        var modifier = 0
+        if (directionIndex < 0) {
+            modifier = 4
+        } else if (directionIndex > 3) {
+            modifier = -4
+        }
+        return directionIndex + modifier
     },
 
     _inBounds(pos) {
         pos.forEach((coordinate) => {
-            console.log(`Checking if move is in bounds... ${coordinate} should be greater than 0 and lower than `, board_width / 2)
             if (board_width / 2 > coordinate && coordinate < 0) {
                 throw new Error('Out of bounds move.')
             }
@@ -73,14 +108,40 @@ function arraySum(array1, array2, array_size = 2) {
     for (i = 0; i < array_size; i++) {
         array_result.push(array1[i] + array2[i])
     }
-    console.log(array_result)
     return array_result
 }
 
+const board = {
+    roverRender: {
+        'N': '^',
+        'E': '>',
+        'S': 'v',
+        'W': '<',
+    },
+    printBoard(roverLogs = []) {
+        if (roverLogs.length) console.log('\n\nPrinting travel log:\n')
 
-rover.moveForward()
-rover.moveForward()
-rover.turnLeft()
-rover.moveForward()
-rover.moveForward()
-rover.turnRight()
+        for (i = 0; i < board_width; i++) {
+            let row = []
+            for (j = 0; j < board_width; j++) {
+                if (roverLogs.length) {
+                    row[j] = '[   ]'
+                    roverLogs.forEach((roverLog) => {
+                        if (roverLog[1] === i && roverLog[0] === j) {
+                            row[j] = `[ ${roverLogs.indexOf(roverLog)} ]`
+                        }
+                    })
+                } else {
+                    row.push((rover.position[1] === i && rover.position[0] === j) ? `[ ${this.roverRender[rover.direction]} ]` : '[   ]')
+                }
+            }
+            console.log(row.join(' '))
+        }
+    }
+}
+
+
+
+// rover.runCommands('ffzzy ')
+rover.runCommands('rffrfflbbbfrff')
+printBoard(rover.travel_log)
