24521b63f4a2f1274feb459e46d1f43d21accf70
Andres Weber <andresmweber@gmail.com>
2019-08-18 01:45:59 -0400

    new commit, image of old state, working before I started messing with Photon.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { GameController } = require('./controllers/controller')
 const { runtimes } = require('./config')
+require("../style/style.css")
 
 var controller = new GameController(10, runtimes[1])
 controller.start()
